05443b91901cf2d05acf48863f7240817a042e89
Arun Vijayarengan <arunprabu.v@hotmail.com>
2023-10-04 15:30:26 +0530

    updated

--- my-rtk-app/src/App.tsx
+++ my-rtk-app/src/App.tsx
@@ -1,14 +1,12 @@
-import logo from "./logo.svg"
 import { Counter } from "./features/counter/Counter"
 import "./App.css"
+import Header from "./components/Header/Header"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-      </header>
+    <div>
+      <Header />
+      <Counter />
     </div>
   )
 }
---
+++ my-rtk-app/src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header>
+      <a href="/">My RTK App</a>
+    </header>
+  )
+}
+
+export default Header
