a6170431d2d1fb8377ad580c8a6f78019dcffa38
Arun Vijayarengan <arunprabu.v@hotmail.com>
2023-10-10 13:01:07 +0530

    updated

--- my-next-app/pages/products/index.tsx
+++ my-next-app/pages/products/index.tsx
@@ -1,34 +1,48 @@
 import Link from "next/link";
-import { useState } from "react";
 
-export default function Products() {
-  console.log('Inside Products Component');
-
-  const [productList, setProductList] = useState([
-    { id: 1, name: "Bread", price: 50 },
-    { id: 2, name: "Butter", price: 100 },
-    { id: 3, name: "Jam", price: 60 },
-    { id: 4, name: "Ghee", price: 120 },
-    { id: 5, name: "Honey", price: 90 },
-  ]);
+// Static Site Generation - SSG Demo
+export default function Products(props: any) {
+  console.log("Inside Products Component");
+  console.log(props);
 
   return (
     <div className="flex min-h-screen flex-col items-center">
       <h1>All Products</h1>
 
       <ul>
-        {productList.map(product => {
+        {props.productList.map( (product: any) => {
           return (
             <li key={product.id}>
-              <Link href="products/1">{product.name}</Link>
+              <Link href={`products/${product.id}`}>{product.name}</Link>
               <p>Rs: {product.price}</p>
               <hr/>
             </li>
           );
         })}
-        
       </ul>
     </div>
   );
 }
 
+// Returning Static Site Generation related data
+export async function getStaticProps() {
+  console.log('Executed by Server');
+  // can connect to db or can connect to rest api
+  // and get the data and return it as props
+
+  return {
+    props: {
+      productList: [
+        { id: 1, name: "Bread", price: 50 },
+        { id: 2, name: "Butter", price: 100 },
+        { id: 3, name: "Jam", price: 60 },
+        { id: 4, name: "Ghee", price: 120 },
+        { id: 5, name: "Honey", price: 120 },
+      ],
+    },
+    // Next.js will attempt to re-generate the page:
+    // - When a request comes in
+    // - At most once every 10 seconds
+    revalidate: 10, // In seconds
+  };
+}
